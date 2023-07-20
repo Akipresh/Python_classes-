592fb17b9ce7d2933d67b075b90e82f0bf2c26ed
akipresh <akivieprecious@gmail.com>
2023-07-20 15:23:17 -0700

    task 14

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    """Converts a Roman Numeral to an integer"""
+def weight_average(my_list=[]):
+    """Returns the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
 
-    if (not isinstance(roman_string, str) or roman_string == 'None'):
-        return 0
-    roman_numerals = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    output = 0
-
-    for index, character in enumerate(roman_string):
-        if roman_numerals.get(character, 0) == 0:
-            return 0
-
-        if (index != (len(roman_string) - 1) and
-                (roman_numerals[roman_string[index]]
-                 < roman_numerals[roman_string[index + 1]])):
-            output += roman_numerals[character] * -1
-
-        else:
-            output += roman_numerals[character]
-
-    return output
+    average = 0
+    size = 0
+    for tup in my_list:
+        average += (tup[0] * tup[1])
+        size += tup[1]
+    return (average / size)
